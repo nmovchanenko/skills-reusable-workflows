9c9213f511af5e1220d41a98220eeca5aa1c1aa1
Mikalai Mauchanenka <mikalai.mauchanenka@theguarantors.com>
2024-04-29 14:25:03 +0200

    update index

--- src/index.js
+++ src/index.js
@@ -36,18 +36,19 @@ async function run() {
         throw new Error('Owner and repo required');
     }
 
-    const dispatchUrl = `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/dispatches`;
+    const [owner, repo] = REPO_NAME.split('/');
+    const dispatchUrl = `https://api.github.com/repos/${owner}/${repo}/dispatches`;
 
     console.log(`Dispatching ${dispatchUrl} with payload`, payload);
 
-    // const res = await axios.post(dispatchUrl, payload, {
-    //     headers: {
-    //         Authorization: `Bearer ${GITHUB_TOKEN}`,
-    //         Accept: "application/vnd.github.v3+json"
-    //     }
-    // });
-    //
-    // return res.status;
+    const res = await axios.post(dispatchUrl, payload, {
+        headers: {
+            Authorization: `Bearer ${GITHUB_TOKEN}`,
+            Accept: "application/vnd.github.v3+json"
+        }
+    });
+
+    return res.status;
 }
 
 run()
